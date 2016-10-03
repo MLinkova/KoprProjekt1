ee2369127e68473c8282924e576e059c9b967d21
PeterGursky <peter.gursky@upjs.sk>
2016-10-03 04:51:31 +0200

    2. zadanie 2. cvičenia

---
+++ kopr2016/src/sk/upjs/ics/kopr2016/cviko02/zadanie2/Zena.java
@@ -0,0 +1,20 @@
+package sk.upjs.ics.kopr2016.cviko02.zadanie2;
+
+import java.util.List;
+
+public class Zena implements Runnable {
+
+	private List<Chlap> chlapi;
+	
+	public Zena(List<Chlap> chlapi) {
+		this.chlapi = chlapi;
+	}
+
+	@Override
+	public void run() {
+		for (Chlap chlap: chlapi) {
+			System.out.println("Žena: hodnotím "+chlap.getId()+". chlapa: " + chlap.toString());
+		}
+	}
+
+}
---
+++ kopr2016/src/sk/upjs/ics/kopr2016/cviko02/zadanie2/Zivot.java
@@ -0,0 +1,22 @@
+package sk.upjs.ics.kopr2016.cviko02.zadanie2;
+
+import java.util.List;
+
+public class Zivot implements Runnable {
+
+	private List<Chlap> chlapi;
+	
+	public Zivot(List<Chlap> chlapi) {
+		this.chlapi = chlapi;
+	}
+
+	@Override
+	public void run() {
+		for (Chlap chlap : chlapi) {
+			chlap.setVek("starý");
+			System.out.println("Život: bijem "+chlap.getId()+". chlapa");
+			chlap.setKrasa("škaredý");
+		}
+	}
+
+}
