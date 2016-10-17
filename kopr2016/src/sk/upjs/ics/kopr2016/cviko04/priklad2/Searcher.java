1e4864f7de03f381d3ad5d376c081f4457f9a56c
ucitelskyPocitacVP3 <student@158.197.35.78>
2016-10-17 12:14:52 +0200

    cviko 4 z 17/10/2016

---
+++ kopr2016/src/sk/upjs/ics/kopr2016/cviko04/priklad2/Searcher.java
@@ -0,0 +1,37 @@
+package sk.upjs.ics.kopr2016.cviko04.priklad2;
+
+import java.io.File;
+import java.util.Queue;
+
+public class Searcher implements Runnable {
+
+	public static final File POISON_PILL = new File("poison.pill");
+	
+	private File rootDir;
+	private Queue<File> queue;
+	
+
+	public Searcher(File rootDir, Queue<File> queue) {
+		this.rootDir = rootDir;
+		this.queue = queue;
+	}
+
+	public void run() {
+		search(rootDir.listFiles());
+		for (int i = 0; i <  WordCounter.NUMBER_OF_FILE_ANALYZERS; i++)
+			queue.offer(POISON_PILL);
+	}
+	
+	private void search(File[] dir) {
+		for (int i = 0;  i < dir.length; i++) {
+			if (dir[i].isDirectory()) {
+				search(dir[i].listFiles());
+			}
+			else {
+				if (dir[i].getName().endsWith(".java")) {
+					queue.offer(dir[i]);
+				}
+			}
+		}
+	}
+}
