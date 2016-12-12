bc1f052db3e104e3cc97db58ea1f2c24962a45f2
PeterGursky <peter.gursky@upjs.sk>
2016-12-12 08:31:58 +0100

    zadanie pre cviko zo swingu

---
+++ kopr2016/src/sk/gursky/kopr2016/cviko08/zadanie/MainForm.java
@@ -0,0 +1,49 @@
+package sk.gursky.kopr2016.cviko08.zadanie;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
+
+public class MainForm extends JFrame {
+
+	private static final long serialVersionUID = 9126407517129194435L;
+
+	private JTextArea textArea;
+	private RedGreenPanel redGreenPanel;
+
+	public MainForm() {
+		textArea = new JTextArea();
+		add(textArea);
+		
+		redGreenPanel = new RedGreenPanel();
+		add(redGreenPanel, BorderLayout.PAGE_END);
+		
+		setSize(new Dimension(640, 480));
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	public void triggerSpellCheck() throws InterruptedException {
+		while(true) {
+			String textField = textArea.getText();
+			SpellChecker spellChecker = new SpellChecker();
+			List<SpellChecker.SpellcheckBoundary> kontrola = spellChecker.check(textField);
+			if (kontrola.isEmpty()) {
+				redGreenPanel.setGreenState(true);  // korektny text
+			}
+			else {
+				redGreenPanel.setGreenState(false); // zly text
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) throws InterruptedException {
+		MainForm form = new MainForm();
+		form.setVisible(true);
+		form.triggerSpellCheck();
+	}
+}
---
+++ kopr2016/src/sk/gursky/kopr2016/cviko08/zadanie/RedGreenPanel.java
@@ -0,0 +1,19 @@
+package sk.gursky.kopr2016.cviko08.zadanie;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class RedGreenPanel extends JPanel {
+	private static final long serialVersionUID = -4210967154633062013L;
+
+	public RedGreenPanel() {
+		setPreferredSize(new Dimension(640, 20));
+	}
+	
+	public void setGreenState(boolean state) {
+		Color color = state ? Color.GREEN : Color.RED;
+		setBackground(color);		
+	}
+}
